074822f9447935efa6c207be228e7c4d47f7760d
AKK2112 <aleckk18@gmail.com>
2021-07-02 21:40:16 -0500

    comments

--- src/Lab_6_Temp_using_tmp36.ino
+++ src/Lab_6_Temp_using_tmp36.ino
@@ -12,39 +12,37 @@ double temperatureC = 0.0;
 
 double temperatureF = 0.0;
 
-// setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
  pinMode(A4, INPUT);
  Serial.begin(9600);
-  display.setup();
-
-	display.clearDisplay();
-	display.display();
+    //setting up display
+    display.setup();
+    display.clearDisplay();
+    display.display();
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
   display.loop();
-uint64_t reading = analogRead(A4);
+  //telling the board where the tmp36 is and what it should do with the values from the temp sensor. 
+  uint64_t reading = analogRead(A4);
 
-double voltage = (reading * 3.3) / 4095.0;
+  double voltage = (reading * 3.3) / 4095.0;
 
-double temperatureC = (voltage - 0.5) * 100;
+  double temperatureC = (voltage - 0.5) * 100;
 
-double temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  double temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 
+    //displaying the temperatures to a mini OLED display for easy reading of the ambient temperature in my room.
+    display.clearDisplay();
 
-display.clearDisplay();
-
-display.display();
-display.setTextSize(1);
-display.setTextColor(WHITE);
-display.setCursor(0,0);
-display.print("Inside temp (C):"); display.println(temperatureC);
-display.display();
-display.print("Inside temp (F):"); display.print(temperatureF);
-display.display();
-Serial.print(temperatureC); Serial.print(","); Serial.println(temperatureF);
+    display.display();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.print("Inside temp (C):"); display.println(temperatureC);
+    display.display();
+    display.print("Inside temp (F):"); display.print(temperatureF);
+    display.display();
+    Serial.print(temperatureC); Serial.print(","); Serial.println(temperatureF);
 delay(500);
 }
